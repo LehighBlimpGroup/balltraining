ed51c8c0080c054b903a960bd19780a5d40c7d83
David Saldaña <dajulian@gmail.com>
2024-04-09 19:37:37 -0400

    Data extraction is seprated from plot

--- hist.py
+++ hist.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-NUMBINS = 20
 
 
-def compute_lab_histogram(image_path, numbins=16):
+
+def compute_lab_histogram(image_path, numbins):
     # Read the image
     image = cv2.imread(image_path)
     if image is None:
@@ -20,28 +20,23 @@ def compute_lab_histogram(image_path, numbins=16):
 
     # Define the number of bins and range for each channel
     channels = [l_channel, a_channel, b_channel]
-    colors = ("black", "green", "blue")
-    channel_names = ['L', 'A', 'B']
-    histRange = [[0, 256], [100,155], [100, 155]]  # L ranges from 0 to 255, A and B from -128 to 127
+      # L ranges from 0 to 255, A and B from -128 to 127
 
-
-    for i, (channel, color, name) in enumerate(zip(channels, colors, channel_names)):
+    histograms = []
+    for i, channel in enumerate(channels):
         # Calculate histogram
         hist = cv2.calcHist([channel], [0], None, [numbins], histRange[i])
-        hist /=  hist.sum()
+        hist /= hist.sum()
         #hist = cv2.calcHist([image], [0], None, [num_bins], [0, 256])
 
-        # Plot histogram
-        plt.subplot(1, 3, i + 1)
-        plt.plot([-128+histRange[i][0] + (histRange[i][1]-histRange[i][0] ) * j / numbins
-                  for j in range(numbins)],hist, '-.',color=color)
-        # plt.bar(range(numbins), hist, width=1.0, color='gray')
-        plt.title(f'{name} channel Histogram')
-        plt.xlabel('Intensity Value')
-        plt.ylabel('Frequency')
+
         #plt.xlim(histRange[i])
 
-    plt.tight_layout()
+        histograms.append(hist)
+
+    return histograms
+
+
 
 
 
@@ -51,10 +46,9 @@ def compute_lab_histogram(image_path, numbins=16):
 
 import os
 
-def read_files_in_directory(folder_path):
-    # Plot histograms for each channel
-    plt.figure(figsize=(10, 5))
-
+def extract_histograms(folder_path, histRange, numbins):
+    # all hist
+    all_hist = []
     # List all files in the specified directory
     try:
         # os.listdir() returns a list of all files and directories in 'directory'
@@ -62,15 +56,45 @@ def read_files_in_directory(folder_path):
         print("Files in directory:", files)
 
         for file in files:
-            compute_lab_histogram(folder_path + "/" + file, numbins=NUMBINS)
+            histogram = compute_lab_histogram(folder_path + "/" + file, numbins=numbins)
+            all_hist.append(histogram)
     except FileNotFoundError:
         print("The directory does not exist.")
 
+
+    return all_hist
+
+
+
+def plot_histograms(histograms, histRange, numbins):
+    colors = ("black", "green", "blue")
+    channel_names = ['L', 'A', 'B']
+
+
+    # Plot histograms for each channel
+    plt.figure(figsize=(10, 5))
+    for lab_hist in histograms:
+        for i, (hist,color, name) in enumerate(zip(lab_hist, colors, channel_names)):
+            # Plot histogram
+            plt.subplot(1, 3, i + 1)
+            plt.plot([-128 + histRange[i][0] + (histRange[i][1] - histRange[i][0]) * j / numbins
+                      for j in range(numbins)], hist, '-.', color=color)
+            # plt.bar(range(numbins), hist, width=1.0, color='gray')
+            plt.title( name+' channel Histogram')
+            plt.xlabel('Intensity Value')
+            plt.ylabel('Frequency')
+
+
+    plt.tight_layout()
     plt.show()
 
 
+
+histRange = [[0, 256], [100, 155], [100, 155]]
+NUM_BINS = 10
 # Specify the path to your folder
 folder_path = "purple"
-read_files_in_directory(folder_path)
+histograms = extract_histograms(folder_path, histRange, NUM_BINS)
+plot_histograms(histograms, histRange, NUM_BINS)
 
 
